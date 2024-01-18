6d8256cd15c93a4a984debd650733d87e7987723
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2024-01-18 02:18:04 +0100

    refactor: HotelTable Footer,Header,Row && add average row

--- src/pages/HotelTable.tsx
+++ src/pages/HotelTable.tsx
@@ -1,34 +1,12 @@
+// HotelTable.tsx
 import { Table } from "@/stories/Table.layout";
 import { useHotelContext } from "./api/hotel.provider";
-import { FilterIcon } from "@/stories/Filter.icon";
-import {
-  formatNumber,
-  formatCurrency,
-  formatPercentage,
-} from "@/utils/formatters";
 import { useColumnSort } from "@/hooks/useColumnSort";
-import { SortableHeaderCell } from "@/stories/SortableHeaderCell";
 import { HotelSummaryMetrics } from "./api/hotel.normalizer";
-
-type Column = {
-  title: string;
-  sortKey: keyof HotelSummaryMetrics;
-};
-
-const columns: Column[] = [
-  { title: "Hotel Name", sortKey: "hotelName" },
-  { title: "Rooms", sortKey: "rooms" },
-  { title: "Occ (%)", sortKey: "occ" },
-  { title: "ADR ($)", sortKey: "adr" },
-  { title: "RevPar ($)", sortKey: "revpar" },
-  { title: "Total Rev ($)", sortKey: "total_revenue" },
-  { title: "GOP ($)", sortKey: "total_hotel_gop" },
-  { title: "GOP Margin (%)", sortKey: "gop_margin" },
-  { title: "EBITDA ($)", sortKey: "total_ebitda" },
-  { title: "EBITDA Margin (%)", sortKey: "ebitda_margin" },
-  { title: "NOI ($)", sortKey: "noi" },
-  { title: "NOI Margin (%)", sortKey: "noi_margin" },
-];
+import { HotelTableHeader, columns } from "./HotelTableHeader";
+import { HotelTableRow } from "./HotelTableRow";
+import { AverageMetrics, HotelTableFooter } from "./HotelTableFooter";
+import { calculateAverage } from "@/utils/formatters";
 
 export const HotelTable = () => {
   const { hotelPerformanceMetrics } = useHotelContext();
@@ -40,59 +18,27 @@ export const HotelTable = () => {
     initialSortKey: null,
   });
 
-  const renderCell = (content: string | number | null | undefined) => {
-    if (content === null || content === undefined || content === "") {
-      return <Table.EmptyCell />;
+  const averageMetrics = columns.reduce((acc, column) => {
+    if (column.sortKey && column.sortKey !== "hotelName") {
+      acc[column.sortKey] = calculateAverage(sortedItems, column.sortKey);
+      return acc;
     }
-    return content;
-  };
+    return acc;
+  }, {} as AverageMetrics);
 
   return (
     <Table>
-      <Table.Header>
-        {columns.map((column) => (
-          <SortableHeaderCell
-            key={column.sortKey}
-            title={column.title}
-            sortKey={column.sortKey}
-            onSort={handleSort}
-            isSorted={sortKey === column.sortKey}
-            sortDirection={sortKey === column.sortKey ? sortDirection : null}
-          />
-        ))}
-      </Table.Header>
+      <HotelTableHeader
+        columns={columns}
+        handleSort={handleSort}
+        sortKey={sortKey}
+        sortDirection={sortDirection}
+      />
       <Table.Body>
-        {sortedItems &&
-          sortedItems.map((hotel, index) => (
-            <Table.Row key={index}>
-              <Table.Cell>{renderCell(hotel.hotelName)}</Table.Cell>
-              <Table.Cell>{renderCell(formatNumber(hotel.rooms))}</Table.Cell>
-              <Table.Cell>{renderCell(formatPercentage(hotel.occ))}</Table.Cell>
-              <Table.Cell>{renderCell(formatCurrency(hotel.adr))}</Table.Cell>
-              <Table.Cell>
-                {renderCell(formatCurrency(hotel.revpar))}
-              </Table.Cell>
-              <Table.Cell>
-                {renderCell(formatCurrency(hotel.total_revenue))}
-              </Table.Cell>
-              <Table.Cell>
-                {renderCell(formatCurrency(hotel.total_hotel_gop))}
-              </Table.Cell>
-              <Table.Cell>
-                {renderCell(formatPercentage(hotel.gop_margin))}
-              </Table.Cell>
-              <Table.Cell>
-                {renderCell(formatCurrency(hotel.total_ebitda))}
-              </Table.Cell>
-              <Table.Cell>
-                {renderCell(formatPercentage(hotel.ebitda_margin))}
-              </Table.Cell>
-              <Table.Cell>{renderCell(formatCurrency(hotel.noi))}</Table.Cell>
-              <Table.Cell>
-                {renderCell(formatPercentage(hotel.noi_margin))}
-              </Table.Cell>
-            </Table.Row>
-          ))}
+        {sortedItems.map((hotel, index) => (
+          <HotelTableRow key={index} hotel={hotel} />
+        ))}
+        <HotelTableFooter averageMetrics={averageMetrics} columns={columns} />
       </Table.Body>
     </Table>
   );
---
+++ src/pages/HotelTableHeader.tsx
@@ -0,0 +1,50 @@
+import { SortableHeaderCell } from "@/stories/SortableHeaderCell";
+import { HotelSummaryMetrics } from "./api/hotel.normalizer";
+import { Table } from "@/stories/Table.layout";
+
+export type Column = {
+  title: string;
+  sortKey: keyof HotelSummaryMetrics;
+};
+
+export const columns: Column[] = [
+  { title: "Hotel Name", sortKey: "hotelName" },
+  { title: "Rooms", sortKey: "rooms" },
+  { title: "Occ (%)", sortKey: "occ" },
+  { title: "ADR ($)", sortKey: "adr" },
+  { title: "RevPar ($)", sortKey: "revpar" },
+  { title: "Total Rev ($)", sortKey: "total_revenue" },
+  { title: "GOP ($)", sortKey: "total_hotel_gop" },
+  { title: "GOP Margin (%)", sortKey: "gop_margin" },
+  { title: "EBITDA ($)", sortKey: "total_ebitda" },
+  { title: "EBITDA Margin (%)", sortKey: "ebitda_margin" },
+  { title: "NOI ($)", sortKey: "noi" },
+  { title: "NOI Margin (%)", sortKey: "noi_margin" },
+];
+
+interface HotelTableHeaderProps {
+  columns: Column[];
+  handleSort: (key: keyof HotelSummaryMetrics) => void;
+  sortKey: keyof HotelSummaryMetrics | null;
+  sortDirection: "asc" | "desc" | null;
+}
+
+export const HotelTableHeader = ({
+  columns,
+  handleSort,
+  sortKey,
+  sortDirection,
+}: HotelTableHeaderProps) => (
+  <Table.Header>
+    {columns.map((column) => (
+      <SortableHeaderCell
+        key={column.sortKey}
+        title={column.title}
+        sortKey={column.sortKey}
+        onSort={handleSort}
+        isSorted={sortKey === column.sortKey}
+        sortDirection={sortKey === column.sortKey ? sortDirection : null}
+      />
+    ))}
+  </Table.Header>
+);
