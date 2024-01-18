12eac2f9f40d31851d80934dbf06f51164a58739
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2024-01-18 01:26:14 +0100

    feat: replace Next.js logo by the table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,16 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { HotelStoreProvider, useHotelStore } from "./api/hotel.provider";
+import { HotelTable } from "./HotelTable";
+import { Suspense } from "react";
+import { ErrorBoundary } from "@/utils/ErrorBondary";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const hotelStore = useHotelStore();
+
   return (
     <>
       <Head>
@@ -39,16 +45,13 @@ export default function Home() {
           </div>
         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
+        <HotelStoreProvider store={hotelStore}>
+          <ErrorBoundary>
+            <Suspense fallback={<div>Loading hotels...</div>}>
+              <HotelTable />
+            </Suspense>
+          </ErrorBoundary>
+        </HotelStoreProvider>
 
         <div className={styles.grid}>
           <a
