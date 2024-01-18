61638465532565ce656c8806462c29d28fe23e5d
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2024-01-18 01:16:30 +0100

    feat: create an ErrorBondary class component

---
+++ src/utils/ErrorBondary.tsx
@@ -0,0 +1,35 @@
+import { Component } from "react";
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+interface ErrorBoundaryProps {
+  children?: React.ReactNode;
+}
+
+export class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo); // --> Sentry
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong.</div>;
+    }
+
+    return this.props.children;
+  }
+}
